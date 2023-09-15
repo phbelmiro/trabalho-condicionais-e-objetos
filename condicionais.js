980bc935dbceea5a41e85030266369edc946d7f1
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-09-15 19:18:39 -0300

    Create condicionais.js

---
+++ condicionais.js
@@ -0,0 +1,92 @@
+/*
+
+*pedro belmiro
+
+A.
+
+1.
+
+A)
+
+B)sim. nao 
+
+
+2.
+
+A) Ver se o animal e carnivoro ou não
+
+B) leão é um animal carnivoro 
+
+3.
+
+A)
+
+B) O numero e maior do que 5. O numero e par.   
+
+*/
+
+//B. 
+
+// let idade = Number(prompt("qual e sua idade?"));
+
+// if (idade >= 18) {
+//     console.log(`você e maior de idade`);
+// }   else {
+//         console.log(`você menor de idade`);
+// }
+
+// let turno = String(prompt('Qual e seu turno [M] matutino [T] tarde [N] noturno'))
+
+// switch(turno) {
+//     case turno === "m": 
+//         console.log('Bom Dia')
+//         break
+//     case turno.toLowerCase() === "t":
+//         console.log('Boa Tarde')
+//         break
+//     case turno.toLowerCase() === "n":
+//         console.log('Boa Noite')
+//         break
+// }
+
+// const aluno = {
+//     nome: 'pedro', 
+//     idade: 17,
+//     tipoDeCurso: 'graduação',
+//     temBolsa: true
+// }
+
+ 
+// if(aluno.idade <=30 && aluno.tipoDeCurso === 'graduação' && aluno.temBolsa === false){
+//     console.log("Você pode se candidatar a uma bolsa")
+// }else{
+//     console.log("Desculpe, você não atende aos requisitos para a bolsa")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
